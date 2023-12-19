8bcb01571f327ffdaedf48134b6e2132722b87ec
rmenneni <rmenneni@ebay.com>
2023-12-18 23:25:50 -0600

    creating a new api automation test repository

---
+++ tests/api_test.spec.js
@@ -0,0 +1,92 @@
+import {test, expect } from '@playwright/test';
+import exp from 'constants';
+
+var _bodyString = `{
+    "data": {
+        "type": "tests",
+        "attributes": {
+            "name": "Test With All the DEVs",
+            "description": "Testing the pass fail status",
+            "priority": "highest",
+            "status": "Pass",
+            "run-status": "Pass"
+        },
+        "steps": {
+            "data": [
+                {
+                    "name": "step one",
+                    "description": "Step 1 description",
+                    "expected-results": "result"
+                },
+                {
+                    "name": "step two",
+                    "expected-results": "result2"
+                }
+            ]
+        }
+    }
+}`;
+
+var _stringForAddress = `{
+    "id": 0,
+    "externalUserId": "6ECA3F97-BB96-423B-B5B7-BD8B8ACDA274",
+    "firstName": "Latonia",
+    "lastName": "Howard",
+    "addressLine1": "7030 NW Beaver Dr",
+    "addressLine2": "",
+    "city": "Johnston",
+    "stateProvinceRegion": "IA",
+    "zipCode": "50131",
+    "countryCode": "US",
+    "phone": "5158670780",
+    "createdAt": "2023-12-13T15:31:28.971Z",
+    "lastUsedAt": "2023-12-13T15:31:28.971Z",
+    "isEasyPostVerified": true,
+    "easyPostShippingAddressId": "",
+    "isDefaultAddress": true
+  }`;
+
+var _bodyJason = JSON.parse(_bodyString);
+var _bodyJasonForAddress = JSON.parse(_stringForAddress);
+
+test('API POST Request', async({request, baseURL}) => {
+    
+    const _response = await request.post(`${baseURL}/projects/20787/tests.json`, { 
+        // data section is for the request body 
+        "data": _bodyJason
+        
+        // We can pass headers here if we want: like below but we will move this into the config
+        //, headers: {}
+    });
+    expect(_response.status()).toBe(200);
+    console.log(await _response.json());
+})
+
+const getAddresses = 'https://addressbook-api.tcgplayer-qa.com/v1/6ECA3F97-BB96-423B-B5B7-BD8B8ACDA274/userAddressBooks';
+
+test('API GET Request', async({request}) => {
+    
+    const _response = await request.get(`${getAddresses}`)
+    
+    //
+    const _responseJson = JSON.parse(await  _response.text())
+    console.log(_responseJson.results[0][0])
+    console.log(_responseJson.results[0][0].firstName)
+    expect(_responseJson.results[0][0].firstName).toBe("Automation")
+})
+
+const _addAddress = 'https://addressbook-api.tcgplayer-qa.com/v1/userAddressBooks/add?ignoreCorrections=true';
+
+test('Address API POST Request', async({request}) => {
+    
+    const _response = await request.post(`${_addAddress}`, { 
+        // data section is for the request body 
+        "data": _bodyJasonForAddress
+        
+        // We can pass headers here if we want: like below but we will move this into the config
+        //, headers: {}
+    });
+    expect(_response.status()).toBe(200);
+    
+    
+})
